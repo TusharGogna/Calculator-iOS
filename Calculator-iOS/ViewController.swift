7000d5cc1d9dc374bc9e6a094c70465464fd6000
Tushar Gogna <36148180+TusharGogna@users.noreply.github.com>
2023-12-21 17:15:28 -0500

    Added digit click methods

--- Calculator-iOS/ViewController.swift
+++ Calculator-iOS/ViewController.swift
@@ -11,73 +11,118 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mCalculatorTxt: UILabel!
   
-    private var calculation = 0
+    private var calculation: Double = 0.0
     private var stringToShow = ""
-    private var isDotAdded = false
+    private var isPartOneDotAdded = false
+    private var isPartTwoDotAdded = false
 
+    private var partOne: Double = 0.0
+    private var partTwo: Double = 0.0
+
+    private var operation = ""
+    
     override func viewDidLoad() {
       
     }
     
     @IBAction func mBtnAC(_ sender: UIButton) {
-        calculation = 0
-        stringToShow = "\(calculation)"
+        partOne = 0
+        partTwo = 0
+        stringToShow = "\(partOne)"
         mCalculatorTxt.text = stringToShow
+        operation = ""
     }
     @IBAction func mBtnPlus(_ sender: UIButton) {
-        
-    }
-    @IBAction func mBtn2(_ sender: UIButton) {
-        if(calculation == 0){
-            if(isDotAdded){
-                isDotAdded = false
-                calculation = 2
-                stringToShow = "\(0.2)"
-            }else{
-                calculation = 2
-                stringToShow = "\(2)"
+        operation = "+"
+        if(partOne != 0.0){
+            if(isPartOneDotAdded){
+                isPartOneDotAdded = false
+                partOne = Double(stringToShow) ?? 0
+                stringToShow.append(".0+")
             }
-        }else{
-            if(isDotAdded){
-                isDotAdded = false
-                calculation = 2
-                stringToShow.append("\(2.2)")
-            }else{
-                stringToShow.append("\(2)")
+            else{
+                 partOne = Double(stringToShow) ?? 0
+                 stringToShow.append("+")
             }
         }
+    }
+    @IBAction func mBtn2(_ sender: UIButton) {
+        enterNumber(value : 2)
         
         mCalculatorTxt.text = stringToShow
     }
+    
     @IBAction func mBtn1(_ sender: UIButton) {
-        if(calculation == 0){
-            calculation = 1
-            stringToShow = "\(1)"
-        }else{
-            stringToShow.append("\(1)")
-        }
+        enterNumber(value : 1)
         
         mCalculatorTxt.text = stringToShow
     }
     
     @IBAction func mBtnEqual(_ sender: UIButton) {
+        performOperation()
     }
     
     @IBAction func mBtnDot(_ sender: UIButton) {
-        if(!stringToShow.contains(".")){
-            isDotAdded = true
+        if(!String(partOne).contains(".")){
+            isPartOneDotAdded = true
+        }
+        if(!String(partTwo).contains(".")){
+            isPartTwoDotAdded = true
         }
     }
     
     @IBAction func mBtn0(_ sender: UIButton) {
-        if(calculation == 0){
-            calculation = 0
-            stringToShow = "\(0)"
-        }else{
-            stringToShow.append("\(0)")
-        }
-        
+        enterNumber(value : 0)
         mCalculatorTxt.text = stringToShow
+    }
+    
+    
+    
+    private func performOperation(){
+        switch(operation){
+        case "+":
+          calculation =   partOne + partTwo
+            stringToShow = "\(calculation)"
+            mCalculatorTxt.text = stringToShow
+         
+        case "-":
+            calculation =   partOne - partTwo
+              stringToShow = "\(calculation)"
+              mCalculatorTxt.text = stringToShow
+        default:
+            printContent("Default Ran")
+        }
+            
+        
+        calculation = 0
+        partOne = 0.0
+        partTwo = 0.0
+        operation = ""
+        stringToShow = "0"
+    }
+    
+    private func enterNumber(value: Int){
+        if(partOne == 0.0){
+            if(isPartOneDotAdded){
+                isPartOneDotAdded = false
+                partOne = Double("0.\(value)") ?? 0
+                stringToShow = "0.\(value)"
+            }else{
+                partOne = Double(value)
+                stringToShow = "\(value)"
+            }
+        }else if(partTwo == 0.0){
+            if(isPartTwoDotAdded){
+                isPartTwoDotAdded = false
+                partTwo = Double("0.\(value)") ?? 0
+                stringToShow = "0.\(value)"
+            }else{
+                partTwo = Double(value)
+                stringToShow = "\(value)"
+            }
+        }  else{
+            stringToShow.append("\(value)")
+        }
     }
      
 }
